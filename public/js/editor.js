6c5ffa8804eed4e20904e6f25cfa9765b47382d2
Patrick <patrick@saoconnell.com>
2014-03-05 09:51:14 -0800

    changes, yo

--- public/js/editor.js
+++ public/js/editor.js
@@ -20,7 +20,10 @@ var shapehash = {};
 
 
 $(document).ready(function(){
-    var sessionstuff = '<%=Session["user"]%>'
+    $('#navbar ul li a').click(function(){
+		$('#navbar ul ul').toggle();
+    });
+    var sessionstuff = '<%=Session["user"]%>';
 	console.log('session: ' + sessionstuff);
 	$('#pageLink').val(window.location.href);
 	
